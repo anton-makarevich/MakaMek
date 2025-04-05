97583ac9e7f83d2c5a556bcf32db4a663cc0f85f
Anton Makarevich <anton.makarevich@gmail.com>
2025-04-06 00:02:33 +0200

    feat: test new UI properties for add player panel

--- tests/MakaMek.Core.Tests/ViewModels/Wrappers/PlayerViewModelTests.cs
+++ tests/MakaMek.Core.Tests/ViewModels/Wrappers/PlayerViewModelTests.cs
@@ -1,4 +1,4 @@
-﻿using Shouldly;
+using Shouldly;
 using Sanet.MakaMek.Core.Models.Game.Players;
 using Sanet.MakaMek.Core.Tests.Data.Community;
 using Sanet.MakaMek.Core.ViewModels.Wrappers;
@@ -88,5 +88,77 @@ public class PlayerViewModelTests
     
         // Assert
         availableUnits.Contains(unit).ShouldBeTrue();
+    }
+
+    [Theory]
+    [InlineData(true, true)]  // Local player can select units
+    [InlineData(false, false)] // Remote player cannot select units
+    public void CanSelectUnits_ShouldReflectLocalPlayerStatus(bool isLocal, bool expected)
+    {
+        // Arrange
+        var playerViewModel = new PlayerViewModel(new Player(Guid.NewGuid(), "Player"), isLocal, []);
+
+        // Act & Assert
+        playerViewModel.CanSelectUnits.ShouldBe(expected);
+    }
+
+    [Theory]
+    [InlineData(true, true)]  // Local player shows add unit controls
+    [InlineData(false, false)] // Remote player does not show add unit controls
+    public void ShowAddUnitControls_ShouldReflectLocalPlayerStatus(bool isLocal, bool expected)
+    {
+        // Arrange
+        var playerViewModel = new PlayerViewModel(new Player(Guid.NewGuid(), "Player"), isLocal, []);
+
+        // Act & Assert
+        playerViewModel.ShowAddUnitControls.ShouldBe(expected);
+    }
+
+    [Theory]
+    [InlineData(true, false)] // Local player does not show read-only list
+    [InlineData(false, true)]  // Remote player shows read-only list
+    public void ShowUnitListReadOnly_ShouldBeInverseOfLocalPlayerStatus(bool isLocal, bool expected)
+    {
+        // Arrange
+        var playerViewModel = new PlayerViewModel(new Player(Guid.NewGuid(), "Player"), isLocal, []);
+
+        // Act & Assert
+        playerViewModel.ShowUnitListReadOnly.ShouldBe(expected);
+    }
+
+    [Theory]
+    [InlineData(true, true)]  // Local player shows join button
+    [InlineData(false, false)] // Remote player does not show join button
+    public void ShowJoinButton_ShouldReflectLocalPlayerStatus(bool isLocal, bool expected)
+    {
+        // Arrange
+        var playerViewModel = new PlayerViewModel(new Player(Guid.NewGuid(), "Player"), isLocal, []);
+
+        // Act & Assert
+        playerViewModel.ShowJoinButton.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void CanJoin_ShouldBeTrue_WhenUnitsAreAdded()
+    {
+        // Arrange
+        var playerViewModel = new PlayerViewModel(new Player(Guid.NewGuid(), "Player"), true, []);
+        var unit = MechFactoryTests.CreateDummyMechData();
+        playerViewModel.SelectedUnit = unit;
+        playerViewModel.AddUnitCommand.Execute(null); // Add a unit
+
+        // Act & Assert
+        playerViewModel.CanJoin.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void CanJoin_ShouldBeFalse_WhenNoUnitsAreAdded()
+    {
+        // Arrange
+        var playerViewModel = new PlayerViewModel(new Player(Guid.NewGuid(), "Player"), true, []);
+        // No units added
+
+        // Act & Assert
+        playerViewModel.CanJoin.ShouldBeFalse();
     }
 }
