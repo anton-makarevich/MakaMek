ef9955d846f88e125abd4efe1b1683d22dc4bad7
Anton Makarevich <anton.makarevich@gmail.com>
2025-04-19 23:31:50 +0200

    feat: group wepons by location

--- src/MakaMek.Avalonia/MakaMek.Avalonia/Controls/UnitWeaponsPanel.axaml.cs
+++ src/MakaMek.Avalonia/MakaMek.Avalonia/Controls/UnitWeaponsPanel.axaml.cs
@@ -4,45 +4,55 @@ using Avalonia.Controls;
 using Sanet.MakaMek.Core.Models.Units;
 using Sanet.MakaMek.Core.Models.Units.Components.Weapons;
 
-namespace Sanet.MakaMek.Avalonia.Controls
+namespace Sanet.MakaMek.Avalonia.Controls;
+
+public partial class UnitWeaponsPanel : UserControl
 {
-    public partial class UnitWeaponsPanel : UserControl
+    public UnitWeaponsPanel()
     {
-        public UnitWeaponsPanel()
+        InitializeComponent();
+    }
+
+    public static readonly DirectProperty<UnitWeaponsPanel, Unit?> UnitProperty =
+        AvaloniaProperty.RegisterDirect<UnitWeaponsPanel, Unit?>(
+            nameof(Unit),
+            o => o.Unit,
+            (o, v) => o.Unit = v);
+
+    private Unit? _unit;
+    public Unit? Unit
+    {
+        get => _unit;
+        set
         {
-            InitializeComponent();
+            SetAndRaise(UnitProperty, ref _unit, value);
+            UpdateWeaponsList();
+        }
+    }
+
+
+
+    private void UpdateWeaponsList()
+    {
+        if (Unit == null)
+        {
+            WeaponsGroup.ItemsSource = null;
+            return;
         }
 
-        public static readonly DirectProperty<UnitWeaponsPanel, Unit?> UnitProperty =
-            AvaloniaProperty.RegisterDirect<UnitWeaponsPanel, Unit?>(
-                nameof(Unit),
-                o => o.Unit,
-                (o, v) => o.Unit = v);
+        var weapons = Unit.Parts
+            .SelectMany(p => p.GetComponents<Weapon>())
+            .ToList();
 
-        private Unit? _unit;
-        public Unit? Unit
-        {
-            get => _unit;
-            set
+        var grouped = weapons
+            .GroupBy(w => w.MountedOn)
+            .Select(g => new WeaponGroup
             {
-                SetAndRaise(UnitProperty, ref _unit, value);
-                UpdateWeaponsList();
-            }
-        }
+                MountedOn = g.Key?.Name??"NA",
+                Weapons = g.ToList()
+            })
+            .ToList();
 
-        private void UpdateWeaponsList()
-        {
-            if (Unit == null)
-            {
-                WeaponsList.ItemsSource = null;
-                return;
-            }
-
-            var weapons = Unit.Parts
-                .SelectMany(p => p.GetComponents<Weapon>())
-                .ToList();
-
-            WeaponsList.ItemsSource = weapons;
-        }
+        WeaponsGroup.ItemsSource = grouped;
     }
 }
---
+++ src/MakaMek.Avalonia/MakaMek.Avalonia/Controls/WeaponGroup.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Sanet.MakaMek.Core.Models.Units.Components.Weapons;
+
+namespace Sanet.MakaMek.Avalonia.Controls;
+
+public class WeaponGroup
+{
+    public string MountedOn { get; set; } = string.Empty;
+    public List<Weapon> Weapons { get; set; } = [];
+}
