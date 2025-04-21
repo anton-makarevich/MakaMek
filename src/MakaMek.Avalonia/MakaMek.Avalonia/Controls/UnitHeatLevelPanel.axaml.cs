984daa42df49e53c301a977ed69bba4deafc95ff
Anton Makarevich <anton.makarevich@gmail.com>
2025-04-21 11:30:57 +0200

    chore: extract mech info panel

---
+++ src/MakaMek.Avalonia/MakaMek.Avalonia/Controls/UnitHeatLevelPanel.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Sanet.MakaMek.Avalonia.Controls;
+
+public partial class UnitHeatLevelPanel : UserControl
+{
+    public UnitHeatLevelPanel()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/MakaMek.Avalonia/MakaMek.Avalonia/Controls/UnitMovementInfoPanel.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Sanet.MakaMek.Avalonia.Controls;
+
+public partial class UnitMovementInfoPanel : UserControl
+{
+    public UnitMovementInfoPanel()
+    {
+        InitializeComponent();
+    }
+}
