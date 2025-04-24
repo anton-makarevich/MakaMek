4c625ca802a16de1dbff2b1f2a2ff29c561f1ea2
Anton Makarevich <anton.makarevich@gmail.com>
2025-04-24 16:49:55 +0200

    feat: apply color to destroyed part

--- src/MakaMek.Avalonia/MakaMek.Avalonia/Controls/UnitHealthIndicator.axaml.cs
+++ src/MakaMek.Avalonia/MakaMek.Avalonia/Controls/UnitHealthIndicator.axaml.cs
@@ -42,96 +42,106 @@ namespace Sanet.MakaMek.Avalonia.Controls
                 switch (part.Location)
                 {
                     case PartLocation.Head:
-                        UpdateBar(HeadArmor, part.CurrentArmor, part.MaxArmor);
-                        UpdateBar(HeadStructure, part.CurrentStructure, part.MaxStructure);
+                        UpdateBar(HeadArmor, part.CurrentArmor, part.MaxArmor, part.IsDestroyed);
+                        UpdateBar(HeadStructure, part.CurrentStructure, part.MaxStructure, part.IsDestroyed);
                         break;
                     case PartLocation.CenterTorso:
-                        UpdateBar(CenterTorsoArmor, part.CurrentArmor, part.MaxArmor);
-                        UpdateBar(CenterTorsoStructure, part.CurrentStructure, part.MaxStructure);
+                        UpdateBar(CenterTorsoArmor, part.CurrentArmor, part.MaxArmor, part.IsDestroyed);
+                        UpdateBar(CenterTorsoStructure, part.CurrentStructure, part.MaxStructure, part.IsDestroyed);
                         if (part is Torso torso)
                         {
-                            UpdateBar(CenterTorsoRearArmor, torso.CurrentRearArmor, torso.MaxRearArmor);
+                            UpdateBar(CenterTorsoRearArmor, torso.CurrentRearArmor, torso.MaxRearArmor, part.IsDestroyed);
                         }
                         break;
                     case PartLocation.LeftTorso:
-                        UpdateBar(LeftTorsoArmor, part.CurrentArmor, part.MaxArmor);
-                        UpdateBar(LeftTorsoStructure, part.CurrentStructure, part.MaxStructure);
+                        UpdateBar(LeftTorsoArmor, part.CurrentArmor, part.MaxArmor, part.IsDestroyed);
+                        UpdateBar(LeftTorsoStructure, part.CurrentStructure, part.MaxStructure, part.IsDestroyed);
                         if (part is Torso torsoLT)
                         {
-                            UpdateBar(LeftTorsoRearArmor, torsoLT.CurrentRearArmor, torsoLT.MaxRearArmor);
+                            UpdateBar(LeftTorsoRearArmor, torsoLT.CurrentRearArmor, torsoLT.MaxRearArmor, part.IsDestroyed);
                         }
                         break;
                     case PartLocation.RightTorso:
-                        UpdateBar(RightTorsoArmor, part.CurrentArmor, part.MaxArmor);
-                        UpdateBar(RightTorsoStructure, part.CurrentStructure, part.MaxStructure);
+                        UpdateBar(RightTorsoArmor, part.CurrentArmor, part.MaxArmor, part.IsDestroyed);
+                        UpdateBar(RightTorsoStructure, part.CurrentStructure, part.MaxStructure, part.IsDestroyed);
                         if (part is Torso torsoRT)
                         {
-                            UpdateBar(RightTorsoRearArmor, torsoRT.CurrentRearArmor, torsoRT.MaxRearArmor);
+                            UpdateBar(RightTorsoRearArmor, torsoRT.CurrentRearArmor, torsoRT.MaxRearArmor, part.IsDestroyed);
                         }
                         break;
                     case PartLocation.LeftArm:
-                        UpdateBar(LeftArmArmor, part.CurrentArmor, part.MaxArmor);
-                        UpdateBar(LeftArmStructure, part.CurrentStructure, part.MaxStructure);
+                        UpdateBar(LeftArmArmor, part.CurrentArmor, part.MaxArmor, part.IsDestroyed);
+                        UpdateBar(LeftArmStructure, part.CurrentStructure, part.MaxStructure, part.IsDestroyed);
                         break;
                     case PartLocation.RightArm:
-                        UpdateBar(RightArmArmor, part.CurrentArmor, part.MaxArmor);
-                        UpdateBar(RightArmStructure, part.CurrentStructure, part.MaxStructure);
+                        UpdateBar(RightArmArmor, part.CurrentArmor, part.MaxArmor, part.IsDestroyed);
+                        UpdateBar(RightArmStructure, part.CurrentStructure, part.MaxStructure, part.IsDestroyed);
                         break;
                     case PartLocation.LeftLeg:
-                        UpdateBar(LeftLegArmor, part.CurrentArmor, part.MaxArmor);
-                        UpdateBar(LeftLegStructure, part.CurrentStructure, part.MaxStructure);
+                        UpdateBar(LeftLegArmor, part.CurrentArmor, part.MaxArmor, part.IsDestroyed);
+                        UpdateBar(LeftLegStructure, part.CurrentStructure, part.MaxStructure, part.IsDestroyed);
                         break;
                     case PartLocation.RightLeg:
-                        UpdateBar(RightLegArmor, part.CurrentArmor, part.MaxArmor);
-                        UpdateBar(RightLegStructure, part.CurrentStructure, part.MaxStructure);
+                        UpdateBar(RightLegArmor, part.CurrentArmor, part.MaxArmor, part.IsDestroyed);
+                        UpdateBar(RightLegStructure, part.CurrentStructure, part.MaxStructure, part.IsDestroyed);
                         break;
                 }
             }
         }
 
-        private void UpdateBar(ProgressBar? bar, int current, int max)
+        private void UpdateBar(ProgressBar? bar, int current, int max, bool isDestroyed = false)
         {
             if (bar == null) return;
             bar.Value = current;
             bar.Maximum = max;
+            
+            // Add or remove the destroyed class based on part status
+            if (isDestroyed)
+            {
+                bar.Classes.Add("destroyed");
+            }
+            else
+            {
+                bar.Classes.Remove("destroyed");
+            }
         }
 
         private void ClearHealthBars()
         {
             // Head
-            UpdateBar(HeadArmor, 0, 1);
-            UpdateBar(HeadStructure, 0, 1);
+            UpdateBar(HeadArmor, 0, 1, false);
+            UpdateBar(HeadStructure, 0, 1, false);
 
             // Center Torso
-            UpdateBar(CenterTorsoArmor, 0, 1);
-            UpdateBar(CenterTorsoStructure, 0, 1);
-            UpdateBar(CenterTorsoRearArmor, 0, 1);
+            UpdateBar(CenterTorsoArmor, 0, 1, false);
+            UpdateBar(CenterTorsoStructure, 0, 1, false);
+            UpdateBar(CenterTorsoRearArmor, 0, 1, false);
 
             // Left Torso
-            UpdateBar(LeftTorsoArmor, 0, 1);
-            UpdateBar(LeftTorsoStructure, 0, 1);
-            UpdateBar(LeftTorsoRearArmor, 0, 1);
+            UpdateBar(LeftTorsoArmor, 0, 1, false);
+            UpdateBar(LeftTorsoStructure, 0, 1, false);
+            UpdateBar(LeftTorsoRearArmor, 0, 1, false);
 
             // Right Torso
-            UpdateBar(RightTorsoArmor, 0, 1);
-            UpdateBar(RightTorsoStructure, 0, 1);
-            UpdateBar(RightTorsoRearArmor, 0, 1);
+            UpdateBar(RightTorsoArmor, 0, 1, false);
+            UpdateBar(RightTorsoStructure, 0, 1, false);
+            UpdateBar(RightTorsoRearArmor, 0, 1, false);
 
             // Left Arm
-            UpdateBar(LeftArmArmor, 0, 1);
-            UpdateBar(LeftArmStructure, 0, 1);
+            UpdateBar(LeftArmArmor, 0, 1, false);
+            UpdateBar(LeftArmStructure, 0, 1, false);
 
             // Right Arm
-            UpdateBar(RightArmArmor, 0, 1);
-            UpdateBar(RightArmStructure, 0, 1);
+            UpdateBar(RightArmArmor, 0, 1, false);
+            UpdateBar(RightArmStructure, 0, 1, false);
 
             // Left Leg
-            UpdateBar(LeftLegArmor, 0, 1);
-            UpdateBar(LeftLegStructure, 0, 1);
+            UpdateBar(LeftLegArmor, 0, 1, false);
+            UpdateBar(LeftLegStructure, 0, 1, false);
 
             // Right Leg
-            UpdateBar(RightLegArmor, 0, 1);
-            UpdateBar(RightLegStructure, 0, 1);
+            UpdateBar(RightLegArmor, 0, 1, false);
+            UpdateBar(RightLegStructure, 0, 1, false);
         }
     }
 }
