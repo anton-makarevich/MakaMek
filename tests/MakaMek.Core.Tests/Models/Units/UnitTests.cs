dcb9d37aae6c80445d96f3dc73c01c57fb300c2d
Anton Makarevich <anton.makarevich@gmail.com>
2025-04-24 10:46:07 +0200

    fix: break loop in the test

--- tests/MakaMek.Core.Tests/Models/Units/UnitTests.cs
+++ tests/MakaMek.Core.Tests/Models/Units/UnitTests.cs
@@ -48,7 +48,10 @@ public class UnitTests
 
         public override bool CanMoveBackward(MovementType type) => true;
 
-        public override PartLocation? GetTransferLocation(PartLocation location) => PartLocation.CenterTorso;
+        public override PartLocation? GetTransferLocation(PartLocation location) =>
+            location==PartLocation.CenterTorso
+                ?null
+                : PartLocation.CenterTorso;
 
         protected override void ApplyHeatEffects()
         {
