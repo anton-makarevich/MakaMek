c32659f6092b8382492f3b5acebac9731df0e024
Anton Makarevich <anton.makarevich@gmail.com>
2025-05-22 00:26:52 +0200

    refactor: split test project

--- src/MakaMek.Presentation/Properties/AssemblyInfo.cs
+++ src/MakaMek.Presentation/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Sanet.MakaMek.Core.Tests")]
+[assembly: InternalsVisibleTo("Sanet.MakaMek.Presentation.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // This is needed for NSubstitute to mock internal members
