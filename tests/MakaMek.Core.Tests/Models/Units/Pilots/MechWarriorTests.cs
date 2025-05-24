e72a8329e64a8c1ee2b6c8c6f1b2ce5fc7e63016
Anton Makarevich <anton.makarevich@gmail.com>
2025-05-25 01:00:05 +0200

    feat: test mewchwarrior injuries

--- tests/MakaMek.Core.Tests/Models/Units/Pilots/MechWarriorTests.cs
+++ tests/MakaMek.Core.Tests/Models/Units/Pilots/MechWarriorTests.cs
@@ -9,27 +9,58 @@ public class MechWarriorTests
     public void Constructor_WithDefaultValues_SetsDefaultSkills()
     {
         // Arrange & Act
-        var pilot = new MechWarrior("John", "Doe");
+        var sut = new MechWarrior("John", "Doe");
 
         // Assert
-        pilot.FirstName.ShouldBe("John");
-        pilot.LastName.ShouldBe("Doe");
-        pilot.Gunnery.ShouldBe(MechWarrior.DefaultGunnery);
-        pilot.Piloting.ShouldBe(MechWarrior.DefaultPiloting);
-        pilot.Health.ShouldBe(MechWarrior.DefaultHealth);
+        sut.FirstName.ShouldBe("John");
+        sut.LastName.ShouldBe("Doe");
+        sut.Gunnery.ShouldBe(MechWarrior.DefaultGunnery);
+        sut.Piloting.ShouldBe(MechWarrior.DefaultPiloting);
+        sut.Health.ShouldBe(MechWarrior.DefaultHealth);
+        sut.Injuries.ShouldBe(0);
+        sut.IsUnconscious.ShouldBe(false);
     }
 
     [Fact]
     public void Constructor_WithCustomSkills_SetsProvidedValues()
     {
         // Arrange & Act
-        var pilot = new MechWarrior("John", "Doe", gunnery: 3, piloting: 4);
+        var sut = new MechWarrior("John", "Doe", gunnery: 3, piloting: 4);
 
         // Assert
-        pilot.FirstName.ShouldBe("John");
-        pilot.LastName.ShouldBe("Doe");
-        pilot.Gunnery.ShouldBe(3);
-        pilot.Piloting.ShouldBe(4);
-        pilot.Health.ShouldBe(MechWarrior.DefaultHealth);
+        sut.FirstName.ShouldBe("John");
+        sut.LastName.ShouldBe("Doe");
+        sut.Gunnery.ShouldBe(3);
+        sut.Piloting.ShouldBe(4);
+        sut.Health.ShouldBe(MechWarrior.DefaultHealth);
+    }
+    
+    [Fact]
+    public void Hit_IncrementsInjuries()
+    {
+        // Arrange
+        var sut = new MechWarrior("John", "Doe");
+        var initialInjuries = sut.Injuries;
+        
+        // Act
+        sut.Hit();
+        
+        // Assert
+        sut.Injuries.ShouldBe(initialInjuries + 1);
+    }
+    
+    [Fact]
+    public void Hit_MultipleTimesIncrementsInjuriesCorrectly()
+    {
+        // Arrange
+        var sut = new MechWarrior("John", "Doe");
+        
+        // Act
+        sut.Hit();
+        sut.Hit();
+        sut.Hit();
+        
+        // Assert
+        sut.Injuries.ShouldBe(3);
     }
 }
