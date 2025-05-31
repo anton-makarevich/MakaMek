a4868c58529590f9f929f770e2329e3b1a0b85e2
Anton Makarevich <anton.makarevich@gmail.com>
2025-05-31 22:06:27 +0200

    chore: fix xml description

--- src/MakaMek.Core/Models/Game/Mechanics/PilotingSkillCalculator.cs
+++ src/MakaMek.Core/Models/Game/Mechanics/PilotingSkillCalculator.cs
@@ -24,7 +24,7 @@ public class PilotingSkillCalculator : IPilotingSkillCalculator
     /// Gets a detailed breakdown of all modifiers affecting the piloting skill roll with additional context
     /// </summary>
     /// <param name="unit">The unit making the piloting skill roll</param>
-    /// <param name="rollTypes">An optional collection of specific Piloting Skill Roll types to consider. If null or empty, all applicable modifiers are calculated.</param>
+    /// <param name="rollTypes">A collection of specific Piloting Skill Roll types to consider.</param>
     /// <param name="map">The battle map, used for terrain-based modifiers</param>
     /// <returns>A breakdown of the piloting skill roll calculation</returns>
     public PsrBreakdown GetPsrBreakdown(Unit unit, IEnumerable<PilotingSkillRollType> rollTypes, BattleMap? map)
