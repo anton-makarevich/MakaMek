26b6f9dce022f0b3156d2869508e81ff11ba0f50
Anton Makarevich <anton.makarevich@gmail.com>
2025-05-31 23:40:22 +0200

    feat: test fall command on leg actuator hit

--- src/MakaMek.Core/Models/Units/Components/Internal/Actuators/LowerLegActuator.cs
+++ src/MakaMek.Core/Models/Units/Components/Internal/Actuators/LowerLegActuator.cs
@@ -2,12 +2,9 @@ using Sanet.MakaMek.Core.Data.Community;
 
 namespace Sanet.MakaMek.Core.Models.Units.Components.Internal.Actuators;
 
-public class LowerLegActuator : Component
+public class LowerLegActuator() : Component("Lower Leg", LowerLegSlots)
 {
     private static readonly int[] LowerLegSlots = [2];
-    public LowerLegActuator() : base("Lower Leg", LowerLegSlots)
-    {
-    }
 
     public override MakaMekComponent ComponentType => MakaMekComponent.LowerLegActuator;
     public override bool IsRemovable => false;
