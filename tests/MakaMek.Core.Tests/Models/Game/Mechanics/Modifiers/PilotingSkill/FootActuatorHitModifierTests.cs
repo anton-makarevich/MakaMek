94db6e826a2b61d7221cb924c0e419c617d9d676
Anton Makarevich <anton.makarevich@gmail.com>
2025-06-30 22:07:31 +0200

    feat: test new modifiers

---
+++ tests/MakaMek.Core.Tests/Models/Game/Mechanics/Modifiers/PilotingSkill/FootActuatorHitModifierTests.cs
@@ -0,0 +1,45 @@
+﻿using NSubstitute;
+using Sanet.MakaMek.Core.Models.Game.Mechanics.Modifiers.PilotingSkill;
+using Sanet.MakaMek.Core.Services.Localization;
+using Shouldly;
+
+namespace Sanet.MakaMek.Core.Tests.Models.Game.Mechanics.Modifiers.PilotingSkill;
+
+public class FootActuatorHitModifierTests
+{
+    private readonly ILocalizationService _localizationService = Substitute.For<ILocalizationService>();
+
+    [Fact]
+    public void Render_ShouldFormatCorrectly()
+    {
+        // Arrange
+        var sut = new FootActuatorHitModifier
+        {
+            Value = 1
+        };
+        _localizationService.GetString("Modifier_FootActuatorHit").Returns("Foot Actuator Hit: +{0}");
+
+        // Act
+        var result = sut.Render(_localizationService);
+
+        // Assert
+        result.ShouldBe("Foot Actuator Hit: +1");
+        _localizationService.Received(1).GetString("Modifier_FootActuatorHit");
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void Value_ShouldBeSetCorrectly(int expectedValue)
+    {
+        // Arrange & Act
+        var sut = new FootActuatorHitModifier
+        {
+            Value = expectedValue
+        };
+
+        // Assert
+        sut.Value.ShouldBe(expectedValue);
+    }
+}
---
+++ tests/MakaMek.Core.Tests/Models/Game/Mechanics/Modifiers/PilotingSkill/LegDestroyedModifierTests.cs
@@ -0,0 +1,45 @@
+﻿using NSubstitute;
+using Sanet.MakaMek.Core.Models.Game.Mechanics.Modifiers.PilotingSkill;
+using Sanet.MakaMek.Core.Services.Localization;
+using Shouldly;
+
+namespace Sanet.MakaMek.Core.Tests.Models.Game.Mechanics.Modifiers.PilotingSkill;
+
+public class LegDestroyedModifierTests
+{
+    private readonly ILocalizationService _localizationService = Substitute.For<ILocalizationService>();
+
+    [Fact]
+    public void Render_ShouldFormatCorrectly()
+    {
+        // Arrange
+        var sut = new LegDestroyedModifier
+        {
+            Value = 5
+        };
+        _localizationService.GetString("Modifier_LegDestroyed").Returns("Leg Destroyed: +{0}");
+
+        // Act
+        var result = sut.Render(_localizationService);
+
+        // Assert
+        result.ShouldBe("Leg Destroyed: +5");
+        _localizationService.Received(1).GetString("Modifier_LegDestroyed");
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(5)]
+    [InlineData(10)]
+    public void Value_ShouldBeSetCorrectly(int expectedValue)
+    {
+        // Arrange & Act
+        var sut = new LegDestroyedModifier
+        {
+            Value = expectedValue
+        };
+
+        // Assert
+        sut.Value.ShouldBe(expectedValue);
+    }
+}
