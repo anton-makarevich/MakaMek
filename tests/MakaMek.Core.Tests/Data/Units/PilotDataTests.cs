e8c67a61f873cda96862ce9a584953230f8b14b8
Anton Makarevich <anton.makarevich@gmail.com>
2025-07-23 22:34:09 +0200

    feat: test default pilot data

---
+++ tests/MakaMek.Core.Tests/Data/Units/PilotDataTests.cs
@@ -0,0 +1,43 @@
+using Shouldly;
+using Sanet.MakaMek.Core.Data.Units;
+
+namespace Sanet.MakaMek.Core.Tests.Data.Units;
+
+public class PilotDataTests
+{
+    [Fact]
+    public void CreateDefaultPilot_ReturnsPilotWithDefaultValues()
+    {
+        // Arrange
+        const string firstName = "Jane";
+        const string lastName = "Smith";
+
+        // Act
+        var pilot = PilotData.CreateDefaultPilot(firstName, lastName);
+
+        // Assert
+        pilot.FirstName.ShouldBe(firstName);
+        pilot.LastName.ShouldBe(lastName);
+        pilot.Id.ShouldNotBe(Guid.Empty);
+        pilot.Gunnery.ShouldBe(4);
+        pilot.Piloting.ShouldBe(5);
+        pilot.Health.ShouldBe(6);
+        pilot.Injuries.ShouldBe(0);
+        pilot.IsConscious.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void CreateDefaultPilot_ReturnsNewInstanceEachTime()
+    {
+        // Arrange
+        const string firstName = "Test";
+        const string lastName = "Pilot";
+
+        // Act
+        var pilot1 = PilotData.CreateDefaultPilot(firstName, lastName);
+        var pilot2 = PilotData.CreateDefaultPilot(firstName, lastName);
+
+        // Assert
+        pilot1.Id.ShouldNotBe(pilot2.Id);
+    }
+}
