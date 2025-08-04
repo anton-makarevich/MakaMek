6caedf92d5aa11215b2c398a22723c356485a05c
Anton Makarevich <anton.makarevich@gmail.com>
2025-08-04 18:51:40 +0200

    refactor: use static init pattern for converters

--- src/MakaMek.Avalonia/MakaMek.Avalonia/Converters/ConsciousnessColorConverter.cs
+++ src/MakaMek.Avalonia/MakaMek.Avalonia/Converters/ConsciousnessColorConverter.cs
@@ -11,32 +11,25 @@ namespace Sanet.MakaMek.Avalonia.Converters;
 /// </summary>
 public class ConsciousnessColorConverter : IValueConverter
 {
-    private readonly IAvaloniaResourcesLocator _resourcesLocator;
+    private static IAvaloniaResourcesLocator? _resourcesLocator;
 
     /// <summary>
-    /// Creates a new instance of ConsciousnessColorConverter with a specific resources locator
+    /// Initializes the converter with the resources locator
     /// </summary>
     /// <param name="resourcesLocator">The resource locator to use</param>
-    public ConsciousnessColorConverter(IAvaloniaResourcesLocator resourcesLocator)
+    public static void Initialize(IAvaloniaResourcesLocator resourcesLocator)
     {
         _resourcesLocator = resourcesLocator;
-    }
-
-    /// <summary>
-    /// Creates a new instance of ConsciousnessColorConverter with the default resources locator
-    /// </summary>
-    public ConsciousnessColorConverter() : this(new AvaloniaResourcesLocator())
-    {
     }
 
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is not bool isConscious)
-            return _resourcesLocator.TryFindResource("WarningColor") ?? Colors.Gray;
+            return _resourcesLocator?.TryFindResource("WarningColor") ?? Colors.Gray;
 
         return isConscious 
-            ? _resourcesLocator.TryFindResource("SuccessColor") ?? Colors.Green
-            : _resourcesLocator.TryFindResource("ErrorColor") ?? Colors.Red;
+            ? _resourcesLocator?.TryFindResource("SuccessColor") ?? Colors.Green
+            : _resourcesLocator?.TryFindResource("ErrorColor") ?? Colors.Red;
     }
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
