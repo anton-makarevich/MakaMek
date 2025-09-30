8d6b2290d204c05e7c4d5b9664c07a733b477632
Anton Makarevich <anton.makarevich@gmail.com>
2025-09-30 21:13:57 +0200

    feat: test player to data convertion

---
+++ tests/MakaMek.Core.Tests/Data/Players/PlayerExtensionTests.cs
@@ -0,0 +1,23 @@
+﻿using Sanet.MakaMek.Core.Data.Game.Players;
+using Sanet.MakaMek.Core.Models.Game.Players;
+using Shouldly;
+
+namespace Sanet.MakaMek.Core.Tests.Data.Players;
+
+public class PlayerExtensionTests
+{
+    [Fact]
+    public void ToData_ShouldConvertPlayerToPlayerData()
+    {
+        // Arrange
+        var player = new Player(Guid.NewGuid(), "Test Player");
+        
+        // Act
+        var data = player.ToData();
+        
+        // Assert
+        data.Id.ShouldBe(player.Id);
+        data.Name.ShouldBe(player.Name);
+        data.Tint.ShouldBe(player.Tint);
+    }
+}
