59bec10de229bbee299080c0ec9aa4a8fe2e8aaf
Anton Makarevich <anton.makarevich@gmail.com>
2025-09-30 21:12:24 +0200

    feat: test default playerdata creation

---
+++ tests/MakaMek.Core.Tests/Data/Players/PlayerDataTests.cs
@@ -0,0 +1,21 @@
+﻿using Sanet.MakaMek.Core.Data.Game.Players;
+using Shouldly;
+
+namespace Sanet.MakaMek.Core.Tests.Data.Players;
+
+public class PlayerDataTests
+{
+    [Fact]
+    public void CreateDefault_ShouldInitializeProperties()
+    {
+        // Act
+        var sut = PlayerData.CreateDefault();
+        
+        // Assert
+        sut.Id.ShouldNotBe(Guid.Empty);
+        sut.Name.ShouldStartWith("Player");
+        sut.Name.Length.ShouldBe(11);
+        int.TryParse(sut.Name[7..], out _).ShouldBeTrue();
+        sut.Tint.ShouldBe("#FFFFFF");
+    }
+}
