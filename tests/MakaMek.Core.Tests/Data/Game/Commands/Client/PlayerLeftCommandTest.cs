e3f559814f77c95776afc3be0fe80be44f70a16a
Anton Makarevich <anton.makarevich@gmail.com>
2025-10-14 22:18:23 +0200

    feat: test case when player is not found

--- tests/MakaMek.Core.Tests/Data/Game/Commands/Client/PlayerLeftCommandTest.cs
+++ tests/MakaMek.Core.Tests/Data/Game/Commands/Client/PlayerLeftCommandTest.cs
@@ -34,4 +34,26 @@ public class PlayerLeftCommandTest
         result.ShouldBe("Player 1 has left the game.");
         localizationService.Received(1).GetString("Command_PlayerLeft");
     }
+    
+    [Fact]
+    public void Render_ShouldReturnDefaultPlayerName_WhenPlayerNotFound()
+    {
+        // Arrange
+        var localizationService = Substitute.For<ILocalizationService>();
+        localizationService.GetString("Command_PlayerLeft").Returns("{0} has left the game.");
+        var sut = new PlayerLeftCommand
+        {
+            GameOriginId = Guid.NewGuid(),
+            PlayerId = Guid.NewGuid(),
+            Timestamp = DateTime.UtcNow
+        };
+        var game = Substitute.For<IGame>();
+        game.Players.Returns(new List<IPlayer> { new Player(Guid.NewGuid(), "Player 2") });
+
+        // Act
+        var result = sut.Render(localizationService, game);
+
+        // Assert
+        result.ShouldBe("Unknown has left the game.");
+    }
 }
