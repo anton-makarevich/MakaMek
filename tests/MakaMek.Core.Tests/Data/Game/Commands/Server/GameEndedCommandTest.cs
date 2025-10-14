f0120b070264e95609d6a400fdc7c1a7b1bb02ce
Anton Makarevich <anton.makarevich@gmail.com>
2025-10-14 21:53:07 +0200

    feat: test game ended command

---
+++ tests/MakaMek.Core.Tests/Data/Game/Commands/Server/GameEndedCommandTest.cs
@@ -0,0 +1,36 @@
+﻿using JetBrains.Annotations;
+using NSubstitute;
+using Sanet.MakaMek.Core.Data.Game.Commands.Server;
+using Sanet.MakaMek.Core.Models.Game;
+using Sanet.MakaMek.Core.Services.Localization;
+using Shouldly;
+
+namespace Sanet.MakaMek.Core.Tests.Data.Game.Commands.Server;
+
+[TestSubject(typeof(GameEndedCommand))]
+public class GameEndedCommandTest
+{
+
+    [Theory]
+    [InlineData(GameEndReason.Unknown)]
+    [InlineData(GameEndReason.Victory)]
+    [InlineData(GameEndReason.PlayersLeft)]
+    public void Render_ShouldFormatCorrectly(GameEndReason reason)
+    {
+        // Arrange
+        var command = new GameEndedCommand
+        {
+            GameOriginId = Guid.NewGuid(),
+            Reason = reason,
+            Timestamp = DateTime.UtcNow
+        };
+        var localizationService = Substitute.For<ILocalizationService>();
+        localizationService.GetString($"Command_GameEnded_{reason}").Returns($"formatted game ended command {reason}");
+
+        // Act
+        var result = command.Render(localizationService, Substitute.For<IGame>());
+
+        // Assert
+        result.ShouldBe($"formatted game ended command {reason}");
+    }
+}
