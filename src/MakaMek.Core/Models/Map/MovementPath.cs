6542424eaf62635e07454cebfa43fe92c89ad295
Anton Makarevich <anton.makarevich@gmail.com>
2025-12-22 20:42:45 +0100

    feat: introduce movement path class

---
+++ src/MakaMek.Core/Models/Map/MovementPath.cs
@@ -0,0 +1,38 @@
+﻿using Sanet.MakaMek.Core.Data.Game;
+
+namespace Sanet.MakaMek.Core.Models.Map;
+
+public class MovementPath
+{
+    public MovementPath(IEnumerable<PathSegment> segments)
+    {
+        Segments = segments.ToList();
+    }
+    
+    public MovementPath(IEnumerable<PathSegmentData> segments)
+    {
+        Segments = segments.Select(s => new PathSegment(s)).ToList();
+    }
+
+    public IReadOnlyList<PathSegment> Segments { get; }
+    
+    public int TotalCost => Segments.Sum(s => s.Cost);
+    
+    public int HexesTraveled => Hexes.Count;
+    
+    public int DistanceCovered => Start.Coordinates.DistanceTo(Destination.Coordinates);
+    
+    public HexPosition Start => Segments[0].From;
+    
+    public HexPosition Destination => Segments[^1].To;
+    
+    public IReadOnlyList<HexCoordinates> Hexes => 
+        new List<HexCoordinates> { Start.Coordinates }
+            .Concat(Segments.Select(s => s.To.Coordinates).ToList())
+            .Distinct()
+            .ToList();
+    
+    public IReadOnlyList<PathSegmentData> ToData() => Segments.Select(s => s.ToData()).ToList();
+    
+    public int TurnsTaken => Segments.Count(s => s.From.Coordinates == s.To.Coordinates);
+}
---
+++ tests/MakaMek.Core.Tests/Models/Map/MovementPathTests.cs
@@ -0,0 +1,59 @@
+﻿using Sanet.MakaMek.Core.Data.Game;
+using Sanet.MakaMek.Core.Data.Map;
+using Sanet.MakaMek.Core.Models.Map;
+using Shouldly;
+
+namespace Sanet.MakaMek.Core.Tests.Models.Map;
+
+public class MovementPathTests
+{
+    [Fact]
+    public void Constructor_ShouldAcceptData_AndSetProperties()
+    {
+        // Arrange
+        IReadOnlyList<PathSegmentData> segments =
+        [
+            new()
+            {
+                From = new HexPositionData
+                {
+                    Coordinates = new HexCoordinateData(1, 1),
+                    Facing = 0
+                },
+                To = new HexPositionData
+                {
+                    Coordinates = new HexCoordinateData(1, 2),
+                    Facing = 0
+                },
+                Cost = 1
+            },
+            new()
+            {
+                From = new HexPositionData
+                {
+                    Coordinates = new HexCoordinateData(1, 2),
+                    Facing = 0
+                },
+                To = new HexPositionData
+                {
+                    Coordinates = new HexCoordinateData(1, 2),
+                    Facing = 1
+                },
+                Cost = 1
+            }
+        ];
+
+        var sut = new MovementPath(segments);
+
+        // Assert
+        sut.Segments.Count.ShouldBe(2);
+        sut.TotalCost.ShouldBe(2);
+        sut.HexesTraveled.ShouldBe(2);
+        sut.DistanceCovered.ShouldBe(1);
+        sut.Start.Coordinates.ShouldBe(new HexCoordinates(1, 1));
+        sut.Destination.Coordinates.ShouldBe(new HexCoordinates(1, 2));
+        sut.Destination.Facing.ShouldBe(HexDirection.TopRight);
+        sut.Hexes.Count.ShouldBe(2);
+        sut.TurnsTaken.ShouldBe(1);
+    }
+}
