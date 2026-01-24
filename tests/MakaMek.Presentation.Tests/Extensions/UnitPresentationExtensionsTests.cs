9746de76f908ee167685abfde07bf589043c6e72
Anton Makarevich <anton.makarevich@gmail.com>
2026-01-25 00:19:34 +0100

    feat: move presentation specific properties to extensions

---
+++ tests/MakaMek.Presentation.Tests/Extensions/UnitPresentationExtensionsTests.cs
@@ -0,0 +1,71 @@
+﻿using Sanet.MakaMek.Core.Data.Units.Components;
+using Sanet.MakaMek.Core.Models.Units;
+using Sanet.MakaMek.Core.Models.Units.Components;
+using Sanet.MakaMek.Core.Models.Units.Components.Engines;
+using Sanet.MakaMek.Core.Models.Units.Mechs;
+using Sanet.MakaMek.Presentation.Extensions;
+using Shouldly;
+
+namespace Sanet.MakaMek.Presentation.Tests.Extensions;
+
+public class UnitPresentationExtensionsTests
+{
+    private static List<UnitPart> CreateBasicPartsData(int engineRating = 100)
+    {
+        var engineData = new ComponentData
+        {
+            Type = MakaMekComponent.Engine,
+            Assignments =
+            [
+                new LocationSlotAssignment(PartLocation.CenterTorso, 0, 3),
+                new LocationSlotAssignment(PartLocation.CenterTorso, 7, 3)
+            ],
+            SpecificData = new EngineStateData(EngineType.Fusion, engineRating)
+        };
+        var centerTorso = new CenterTorso("CenterTorso", 31, 10, 6);
+        centerTorso.TryAddComponent(new Engine(engineData), [0, 1, 2, 7, 8, 9]).ShouldBeTrue();
+        return
+        [
+            new Head("Head", 9, 3),
+            centerTorso,
+            new SideTorso("LeftTorso", PartLocation.LeftTorso, 25, 8, 6),
+            new SideTorso("RightTorso", PartLocation.RightTorso, 25, 8, 6),
+            new Arm("RightArm", PartLocation.RightArm, 17, 6),
+            new Arm("LeftArm", PartLocation.LeftArm, 17, 6),
+            new Leg("RightLeg", PartLocation.RightLeg, 25, 8),
+            new Leg("LeftLeg", PartLocation.LeftLeg, 25, 8)
+        ];
+    }
+    
+    [Theory]
+    [InlineData(5, 8, 2)]
+    [InlineData(4, 6, 0)]
+    [InlineData(3, 5, 2)]
+    public void GetMovement_ReturnsCorrectMPs(int walkMp, int runMp, int jumpMp)
+    {
+        // Arrange
+        var parts = CreateBasicPartsData(walkMp*50);
+        if (jumpMp > 0)
+        {
+            var centerTorso = parts.Single(p => p.Location == PartLocation.CenterTorso);
+            centerTorso.TryAddComponent(new JumpJets());
+            centerTorso.TryAddComponent(new JumpJets());
+        }
+
+        var mech = new Mech("Test", "TST-1A", 50, parts);
+
+        // Act
+        var walkingMp = mech.GetMovementPoints(MovementType.Walk);
+        var runningMp = mech.GetMovementPoints(MovementType.Run);
+        var jumpingMp = mech.GetMovementPoints(MovementType.Jump);
+
+        // Assert
+        walkingMp.ShouldBe(walkMp, "walking MP should match the base movement");
+        runningMp.ShouldBe(runMp, "running MP should be 1.5x walking");
+        jumpingMp.ShouldBe(jumpMp, "jumping MP should match the number of jump jets");
+        
+        mech.AvailableWalkingPoints.ShouldBe(walkingMp);
+        mech.AvailableRunningPoints.ShouldBe(runningMp);
+        mech.AvailableJumpingPoints.ShouldBe(jumpingMp);
+    }
+}
