a039b17dda03da8b644aed37788c8d07e2ad1536
Anton Makarevich <anton.makarevich@gmail.com>
2026-03-28 22:34:48 +0100

    refactor: use binding instead of code behind

--- src/MakaMek.Avalonia/MakaMek.Avalonia/Views/BattleMapView.axaml.cs
+++ src/MakaMek.Avalonia/MakaMek.Avalonia/Views/BattleMapView.axaml.cs
@@ -143,36 +143,6 @@ public partial class BattleMapView : BaseView<BattleMapViewModel>
                 RenderMap(ViewModel.Game);
             }
         }
-        else if (e.PropertyName == nameof(ViewModel.AvailableActions))
-        {
-            UpdateMobileActionButtons();
-        }
-    }
-
-    private void UpdateMobileActionButtons()
-    {
-        if (ViewModel is not { IsMobile: true }) return;
-
-        MobileActionButtonsPanel.Children.Clear();
-
-        foreach (var action in ViewModel.AvailableActions)
-        {
-            if (!action.IsVisible) continue;
-
-            var button = new Button
-            {
-                Content = action.Label,
-                Padding = new Thickness(20, 12),
-                HorizontalAlignment = HorizontalAlignment.Center,
-                FontSize = 16,
-                FontWeight = FontWeight.Bold,
-                Background = new SolidColorBrush(Colors.Aqua)
-            };
-
-            var capturedAction = action;
-            button.Click += (_, _) => capturedAction.OnExecute();
-            MobileActionButtonsPanel.Children.Add(button);
-        }
     }
 
     private void UpdateMovementPath()
