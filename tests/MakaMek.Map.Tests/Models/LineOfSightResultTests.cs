90b4f273a59ad57ac44032e5645bac9f605151a4
Anton Makarevich <anton.makarevich@gmail.com>
2026-03-30 07:31:01 +0200

    feat: test default result

--- tests/MakaMek.Map.Tests/Models/LineOfSightResultTests.cs
+++ tests/MakaMek.Map.Tests/Models/LineOfSightResultTests.cs
@@ -30,7 +30,7 @@ public class LineOfSightResultTests
         };
         
         // Act
-        var result = LineOfSightResult.Blocked(from,
+        var sut = LineOfSightResult.Blocked(from,
             to,
             new HexCoordinates(2,
                 2),
@@ -40,13 +40,13 @@ public class LineOfSightResultTests
             hexPath);
         
         // Assert
-        result.HasLineOfSight.ShouldBeFalse();
-        result.From.ShouldBe(from);
-        result.To.ShouldBe(to);
-        result.BlockingHexCoordinates.ShouldBe(new HexCoordinates(2, 2));
-        result.BlockReason.ShouldBe(LineOfSightBlockReason.Elevation);
-        result.TotalInterveningFactor.ShouldBe(5);
-        result.HexPath.ShouldBeSameAs(hexPath);
+        sut.HasLineOfSight.ShouldBeFalse();
+        sut.From.ShouldBe(from);
+        sut.To.ShouldBe(to);
+        sut.BlockingHexCoordinates.ShouldBe(new HexCoordinates(2, 2));
+        sut.BlockReason.ShouldBe(LineOfSightBlockReason.Elevation);
+        sut.TotalInterveningFactor.ShouldBe(5);
+        sut.HexPath.ShouldBeSameAs(hexPath);
     }
     
     [Fact]
@@ -74,19 +74,31 @@ public class LineOfSightResultTests
         };
         
         // Act
-        var result = LineOfSightResult.Unblocked(from,
+        var sut = LineOfSightResult.Unblocked(from,
             to,
             1,
             1,
             hexPath);
         
         // Assert
-        result.HasLineOfSight.ShouldBeTrue();
-        result.From.ShouldBe(from);
-        result.To.ShouldBe(to);
-        result.BlockingHexCoordinates.ShouldBeNull();
-        result.BlockReason.ShouldBeNull();
-        result.TotalInterveningFactor.ShouldBe(5);
-        result.HexPath.ShouldBeSameAs(hexPath);
+        sut.HasLineOfSight.ShouldBeTrue();
+        sut.From.ShouldBe(from);
+        sut.To.ShouldBe(to);
+        sut.BlockingHexCoordinates.ShouldBeNull();
+        sut.BlockReason.ShouldBeNull();
+        sut.TotalInterveningFactor.ShouldBe(5);
+        sut.HexPath.ShouldBeSameAs(hexPath);
+    }
+    
+    [Fact]
+    public void Blocked_WithNullHexPath_DefaultsToEmptyPath()
+    {
+        var sut = LineOfSightResult.Blocked(
+            new HexCoordinates(1, 1),
+            new HexCoordinates(3, 3),
+            new HexCoordinates(2, 2));
+    
+        sut.HexPath.ShouldBeEmpty();
+        sut.TotalInterveningFactor.ShouldBe(0);
     }
 }
