eed948a04a01683d1d0825ee1dec6bfed455031a
Anton Makarevich <anton.makarevich@gmail.com>
2026-04-06 23:15:28 +0200

    chore: make text smaller

--- src/MakaMek.Avalonia/MakaMek.Avalonia.Controls/HexControl.cs
+++ src/MakaMek.Avalonia/MakaMek.Avalonia.Controls/HexControl.cs
@@ -169,7 +169,11 @@ public class HexControl : Panel
             VerticalAlignment = VerticalAlignment.Center,
             HorizontalAlignment = HorizontalAlignment.Center,
             Foreground = Brushes.White,
-            FontSize = 12
+            FontSize = 9,
+            TextWrapping = TextWrapping.Wrap,
+            TextTrimming = TextTrimming.CharacterEllipsis,
+            MaxHeight = HexCoordinatesPixelExtensions.HexWidth-10,
+            MaxWidth = HexCoordinatesPixelExtensions.HexWidth-10,  // constrain width so wrapping actually triggers
         };
 
         Children.Add(_highlightTextLabel);
