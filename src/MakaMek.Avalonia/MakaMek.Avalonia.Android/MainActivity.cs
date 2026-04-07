c336ae1ab1c49c4aa0a0078aa01fa65d9221deb6
Anton Makarevich <anton.makarevich@hotmail.com>
2026-04-07 21:45:13 +0200

    feat: support new avalonia android lifecycle model

--- src/MakaMek.Avalonia/MakaMek.Avalonia.Android/MainActivity.cs
+++ src/MakaMek.Avalonia/MakaMek.Avalonia.Android/MainActivity.cs
@@ -2,10 +2,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Android.Views;
-using Avalonia;
 using Avalonia.Android;
-using Sanet.MakaMek.Avalonia.Android.DependencyInjection;
-using Sanet.MVVM.DI.Avalonia.Extensions;
 
 namespace Sanet.MakaMek.Avalonia.Android;
 
@@ -15,15 +12,8 @@ namespace Sanet.MakaMek.Avalonia.Android;
     Icon = "@drawable/icon",
     MainLauncher = true,
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
-public class MainActivity : AvaloniaMainActivity<App>
-{
-    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
-    {
-        return base.CustomizeAppBuilder(builder)
-            .UseDependencyInjection(services=>services.RegisterAndroidServices())
-            .WithInterFont();
-    }
-
+public class MainActivity : AvaloniaMainActivity
+{ 
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
