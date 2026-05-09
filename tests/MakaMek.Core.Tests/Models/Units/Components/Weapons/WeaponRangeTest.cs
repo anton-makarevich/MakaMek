a3f30a0a6fad3468d95a591a72dea704920800f7
Anton Makarevich <anton.makarevich@gmail.com>
2026-05-09 20:59:40 +0200

    refactor: move range value to range bracket

---
+++ tests/MakaMek.Core.Tests/Models/Units/Components/Weapons/WeaponRangeTest.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+using Sanet.MakaMek.Core.Models.Units.Components.Weapons;
+
+namespace Sanet.MakaMek.Core.Tests.Models.Units.Components.Weapons;
+
+[TestSubject(typeof(WeaponRange))]
+public class WeaponRangeTest
+{
+
+    
+}
