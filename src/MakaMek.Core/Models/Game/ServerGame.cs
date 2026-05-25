96bdeb815d205d46db8b208c1e26f5060b732d9f
Anton Makarevich <anton.makarevich@gmail.com>
2026-05-25 14:06:38 +0200

    fix: record command before early return

--- src/MakaMek.Core/Models/Game/ServerGame.cs
+++ src/MakaMek.Core/Models/Game/ServerGame.cs
@@ -149,6 +149,7 @@ public class ServerGame : BaseGame, IDisposable
         if (command is PlayerLeftCommand playerLeftCommand)
         {
             OnPlayerLeft(playerLeftCommand);
+            RecordCommand(command);
             return;
         }
 
