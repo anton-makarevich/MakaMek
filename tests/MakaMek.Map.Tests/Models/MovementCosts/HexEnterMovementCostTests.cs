cce92c3e0d894ce82e7e58fda0e02adafe52fa5b
Anton Makarevich <anton.makarevich@hotmail.com>
2026-06-10 19:43:19 +0200

    fix: add missing file

---
+++ tests/MakaMek.Map.Tests/Models/MovementCosts/HexEnterMovementCostTests.cs
@@ -0,0 +1,29 @@
+using NSubstitute;
+using Sanet.MakaMek.Localization;
+using Sanet.MakaMek.Map.Models.MovementCosts;
+using Shouldly;
+
+namespace Sanet.MakaMek.Map.Tests.Models.MovementCosts;
+
+public class HexEnterMovementCostTests
+{
+    [Fact]
+    public void Value_IsAlways1()
+    {
+        var sut = new HexEnterMovementCost { Value = 1 };
+
+        sut.Value.ShouldBe(1);
+    }
+
+    [Fact]
+    public void Render_ReturnsFormattedString()
+    {
+        var localization = Substitute.For<ILocalizationService>();
+        localization.GetString("MovementCost_HexEnter").Returns("entered hex, {0} MP");
+        var sut = new HexEnterMovementCost { Value = 1 };
+
+        var result = sut.Render(localization);
+
+        result.ShouldBe("entered hex, 1 MP");
+    }
+}
