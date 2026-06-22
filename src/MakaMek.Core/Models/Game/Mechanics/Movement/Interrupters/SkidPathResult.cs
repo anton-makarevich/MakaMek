9f2a0134adcad59ac921738a0ccef633e088d376
Anton Makarevich <anton.makarevich@gmail.com>
2026-06-22 20:32:15 +0200

    chore: ensure record is immutable

--- src/MakaMek.Core/Models/Game/Mechanics/Movement/Interrupters/SkidPathResult.cs
+++ src/MakaMek.Core/Models/Game/Mechanics/Movement/Interrupters/SkidPathResult.cs
@@ -3,7 +3,7 @@
 namespace Sanet.MakaMek.Core.Models.Game.Mechanics.Movement.Interrupters;
 
 internal record SkidPathResult(
-    List<PathSegment> Segments,
+    IReadOnlyList<PathSegment> Segments,
     bool HasCliffFall,
     int LevelsFallen
 );
