8007d9381958944554a6715574a8157ecb87eca2
Anton Makarevich <anton.makarevich@gmail.com>
2026-07-23 12:18:55 +0200

    feat: test room directly

---
+++ tests/MakaMek.Hub.Tests/Rooms/RoomTests.cs
@@ -0,0 +1,59 @@
+using Sanet.MakaMek.Hub.Rooms;
+using Shouldly;
+
+namespace Sanet.MakaMek.Hub.Tests.Rooms;
+
+public class RoomTests
+{
+    private static readonly DateTimeOffset DefaultNow = new(2026, 7, 20, 12, 0, 0, TimeSpan.Zero);
+    private static readonly TimeSpan DefaultTtl = TimeSpan.FromHours(2);
+
+    [Fact]
+    public void RemoveMember_HostPlayerId_ReturnsFalse()
+    {
+        var hostId = Guid.NewGuid();
+        var room = CreateRoom(hostId);
+
+        var result = room.RemoveMember(hostId);
+
+        result.ShouldBeFalse();
+        room.IsMember(hostId).ShouldBeTrue();
+        room.Members.Count.ShouldBe(1);
+    }
+
+    [Fact]
+    public void RemoveMember_NonMember_ReturnsFalse()
+    {
+        var hostId = Guid.NewGuid();
+        var room = CreateRoom(hostId);
+
+        var result = room.RemoveMember(Guid.NewGuid());
+
+        result.ShouldBeFalse();
+        room.Members.Count.ShouldBe(1);
+    }
+
+    [Fact]
+    public void RemoveMember_ClientMember_RemovesMemberAndRevokesSessions()
+    {
+        var hostId = Guid.NewGuid();
+        var clientId = Guid.NewGuid();
+        var room = CreateRoom(hostId);
+        var clientSession = room.AddClientMember("Grace", clientId, DefaultNow, DefaultTtl, () => "client-token");
+
+        var result = room.RemoveMember(clientId);
+
+        result.ShouldBeTrue();
+        room.IsMember(clientId).ShouldBeFalse();
+        room.HasSession(clientSession.Token).ShouldBeFalse();
+        room.Members.Count.ShouldBe(1);
+        room.IsMember(hostId).ShouldBeTrue();
+    }
+
+    private static Room CreateRoom(Guid hostId)
+    {
+        var hostMember = new RoomMember(hostId, "Ada", RoomRole.Host, DefaultNow);
+        var hostSession = new RoomSession("host-token", "ABC234", hostId, RoomRole.Host, DefaultNow.Add(DefaultTtl));
+        return new Room("ABC234", hostMember, hostSession, DefaultNow, DefaultNow.Add(DefaultTtl));
+    }
+}
