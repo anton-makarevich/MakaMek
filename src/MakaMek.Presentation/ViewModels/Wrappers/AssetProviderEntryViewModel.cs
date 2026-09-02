a2d8536b25a4afc6049846cadd9239e61274386b
Anton Makarevich <anton.makarevich@gmail.com>
2026-09-02 14:39:34 +0200

    feat: introduce assets manager vm

---
+++ src/MakaMek.Presentation/ViewModels/Wrappers/AssetProviderEntryViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System.Windows.Input;
+using AsyncAwaitBestPractices.MVVM;
+using Sanet.MakaMek.Assets.Configuration;
+using Sanet.MVVM.Core.ViewModels;
+
+namespace Sanet.MakaMek.Presentation.ViewModels.Wrappers;
+
+public class AssetProviderEntryViewModel : BindableBase
+{
+    private readonly AssetProviderConfigData _provider;
+    private readonly Action<AssetProviderEntryViewModel>? _onToggleActive;
+    private readonly Action<AssetProviderEntryViewModel>? _onRemove;
+    private bool _isActive;
+    private bool _canDeactivate = true;
+
+    public AssetProviderEntryViewModel(
+        AssetProviderConfigData provider,
+        Action<AssetProviderEntryViewModel>? onToggleActive = null,
+        Action<AssetProviderEntryViewModel>? onRemove = null)
+    {
+        _provider = provider;
+        _onToggleActive = onToggleActive;
+        _onRemove = onRemove;
+        _isActive = provider.IsActive;
+
+        ToggleActiveCommand = new AsyncCommand(ExecuteToggleActive);
+        RemoveCommand = new AsyncCommand(ExecuteRemove);
+    }
+
+    public string Id => _provider.Id;
+    public string UrlOrPath => _provider.UrlOrPath;
+    public ProviderType ProviderType => _provider.ProviderType;
+    public AssetType AssetType => _provider.AssetType;
+    public bool IsDefault => _provider.IsDefault;
+    public int SortOrder => _provider.SortOrder;
+
+    public bool IsActive
+    {
+        get => _isActive;
+        set
+        {
+            if (_isActive == value) return;
+            _isActive = value;
+            NotifyPropertyChanged();
+        }
+    }
+
+    public bool CanDeactivate
+    {
+        get => _canDeactivate;
+        set
+        {
+            if (_canDeactivate == value) return;
+            _canDeactivate = value;
+            NotifyPropertyChanged();
+        }
+    }
+
+    public bool CanRemove => !IsDefault;
+
+    public ICommand ToggleActiveCommand { get; }
+    public ICommand RemoveCommand { get; }
+
+    private Task ExecuteToggleActive()
+    {
+        _onToggleActive?.Invoke(this);
+        return Task.CompletedTask;
+    }
+
+    private Task ExecuteRemove()
+    {
+        _onRemove?.Invoke(this);
+        return Task.CompletedTask;
+    }
+}
---
+++ tests/MakaMek.Presentation.Tests/ViewModels/Wrappers/AssetProviderEntryViewModelTests.cs
@@ -0,0 +1,117 @@
+﻿using AsyncAwaitBestPractices.MVVM;
+using Sanet.MakaMek.Assets.Configuration;
+using Sanet.MakaMek.Presentation.ViewModels.Wrappers;
+using Shouldly;
+
+namespace Sanet.MakaMek.Presentation.Tests.ViewModels.Wrappers;
+
+public class AssetProviderEntryViewModelTests
+{
+    private static AssetProviderConfigData Provider(
+        string id,
+        AssetType assetType = AssetType.Units,
+        bool isActive = true,
+        bool isDefault = false) =>
+        new(id, ProviderType.Filesystem, assetType, "/assets/" + id, isActive, isDefault, 0);
+
+    [Fact]
+    public void Constructor_ExposesProviderProperties()
+    {
+        var provider = new AssetProviderConfigData(
+            "local", ProviderType.Filesystem, AssetType.Hexes, "C:\\assets", IsActive: true, IsDefault: false, SortOrder: 3);
+
+        var sut = new AssetProviderEntryViewModel(provider);
+
+        sut.Id.ShouldBe("local");
+        sut.ProviderType.ShouldBe(ProviderType.Filesystem);
+        sut.AssetType.ShouldBe(AssetType.Hexes);
+        sut.UrlOrPath.ShouldBe("C:\\assets");
+        sut.IsDefault.ShouldBeFalse();
+        sut.SortOrder.ShouldBe(3);
+        sut.IsActive.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Constructor_WhenDefault_CanRemoveIsFalse()
+    {
+        var sut = new AssetProviderEntryViewModel(Provider("bucket", isDefault: true));
+
+        sut.CanRemove.ShouldBeFalse();
+    }
+
+    [Fact]
+    public void Constructor_WhenNotDefault_CanRemoveIsTrue()
+    {
+        var sut = new AssetProviderEntryViewModel(Provider("local", isDefault: false));
+
+        sut.CanRemove.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void CanDeactivate_DefaultsToTrue()
+    {
+        var sut = new AssetProviderEntryViewModel(Provider("a"));
+
+        sut.CanDeactivate.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void CanDeactivate_Set_RaisesPropertyChanged()
+    {
+        var sut = new AssetProviderEntryViewModel(Provider("a"));
+        string? changed = null;
+        sut.PropertyChanged += (_, e) => changed = e.PropertyName;
+
+        sut.CanDeactivate = false;
+
+        changed.ShouldBe(nameof(AssetProviderEntryViewModel.CanDeactivate));
+        sut.CanDeactivate.ShouldBeFalse();
+    }
+
+    [Fact]
+    public void IsActive_Set_RaisesPropertyChanged()
+    {
+        var sut = new AssetProviderEntryViewModel(Provider("a"));
+        string? changed = null;
+        sut.PropertyChanged += (_, e) => changed = e.PropertyName;
+
+        sut.IsActive = false;
+
+        changed.ShouldBe(nameof(AssetProviderEntryViewModel.IsActive));
+        sut.IsActive.ShouldBeFalse();
+    }
+
+    [Fact]
+    public void IsActive_SetToSameValue_DoesNotRaisePropertyChanged()
+    {
+        var sut = new AssetProviderEntryViewModel(Provider("a"));
+        var raised = false;
+        sut.PropertyChanged += (_, _) => raised = true;
+
+        sut.IsActive = true;
+
+        raised.ShouldBeFalse();
+    }
+
+    [Fact]
+    public async Task ToggleActiveCommand_WhenExecuted_InvokesOnToggleActive()
+    {
+        AssetProviderEntryViewModel? toggled = null;
+        var sut = new AssetProviderEntryViewModel(Provider("a"), onToggleActive: e => toggled = e);
+
+        await ((IAsyncCommand)sut.ToggleActiveCommand).ExecuteAsync();
+
+        toggled.ShouldBe(sut);
+    }
+
+    [Fact]
+    public async Task RemoveCommand_WhenExecuted_InvokesOnRemove()
+    {
+        AssetProviderEntryViewModel? removed = null;
+        var sut = new AssetProviderEntryViewModel(Provider("a"), onRemove: e => removed = e);
+
+        await ((IAsyncCommand)sut.RemoveCommand).ExecuteAsync();
+
+        removed.ShouldBe(sut);
+    }
+}
