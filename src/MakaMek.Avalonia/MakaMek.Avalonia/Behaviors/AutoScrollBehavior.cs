8590eb19f13532b48c51f33cb9c4110ec24497cc
Anton Makarevich <anton.makarevich@gmail.com>
2026-09-14 18:28:34 +0200

    chore: use virtualizing lists

--- src/MakaMek.Avalonia/MakaMek.Avalonia/Behaviors/AutoScrollBehavior.cs
+++ src/MakaMek.Avalonia/MakaMek.Avalonia/Behaviors/AutoScrollBehavior.cs
@@ -19,9 +19,21 @@ public static class AutoScrollBehavior
     public static void SetEnableAutoScroll(ScrollViewer element, bool value) =>
         element.SetValue(EnableAutoScrollProperty, value);
 
+    public static readonly AttachedProperty<bool> EnableListBoxAutoScrollProperty =
+        AvaloniaProperty.RegisterAttached<ListBox, bool>(
+            "EnableListBoxAutoScroll",
+            typeof(AutoScrollBehavior));
+
+    public static bool GetEnableListBoxAutoScroll(ListBox element) =>
+        element.GetValue(EnableListBoxAutoScrollProperty);
+
+    public static void SetEnableListBoxAutoScroll(ListBox element, bool value) =>
+        element.SetValue(EnableListBoxAutoScrollProperty, value);
+
     static AutoScrollBehavior()
     {
         EnableAutoScrollProperty.Changed.AddClassHandler<ScrollViewer>(OnEnableAutoScrollChanged);
+        EnableListBoxAutoScrollProperty.Changed.AddClassHandler<ListBox>(OnEnableListBoxAutoScrollChanged);
     }
 
     private static void OnEnableAutoScrollChanged(ScrollViewer scrollViewer, AvaloniaPropertyChangedEventArgs e)
@@ -38,8 +50,21 @@ public static class AutoScrollBehavior
         }
     }
 
+    private static void OnEnableListBoxAutoScrollChanged(ListBox listBox, AvaloniaPropertyChangedEventArgs e)
+    {
+        if (e.NewValue is true)
+        {
+            SubscribeToListBoxCollection(listBox);
+        }
+        else
+        {
+            UnsubscribeFromListBoxCollection(listBox);
+        }
+    }
+
     // Store per-instance state using a ConditionalWeakTable
     private static readonly System.Runtime.CompilerServices.ConditionalWeakTable<ScrollViewer, CollectionState> States = new();
+    private static readonly System.Runtime.CompilerServices.ConditionalWeakTable<ListBox, ListBoxCollectionState> ListBoxStates = new();
 
     private static void OnScrollViewerPropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
     {
@@ -74,6 +99,28 @@ public static class AutoScrollBehavior
         }
     }
 
+    private static void SubscribeToListBoxCollection(ListBox listBox)
+    {
+        UnsubscribeFromListBoxCollection(listBox);
+
+        if (listBox.Items is INotifyCollectionChanged collection)
+        {
+            var state = ListBoxStates.GetOrCreateValue(listBox);
+            state.Collection = collection;
+            state.ListBox = listBox;
+            collection.CollectionChanged += state.OnCollectionChanged;
+        }
+    }
+
+    private static void UnsubscribeFromListBoxCollection(ListBox listBox)
+    {
+        if (ListBoxStates.TryGetValue(listBox, out var state) && state.Collection != null)
+        {
+            state.Collection.CollectionChanged -= state.OnCollectionChanged;
+            state.Collection = null;
+        }
+    }
+
     private sealed class CollectionState
     {
         public INotifyCollectionChanged? Collection { get; set; }
@@ -84,6 +131,20 @@ public static class AutoScrollBehavior
             if (e.Action == NotifyCollectionChangedAction.Add)
             {
                 ScrollViewer?.ScrollToEnd();
+            }
+        }
+    }
+
+    private sealed class ListBoxCollectionState
+    {
+        public INotifyCollectionChanged? Collection { get; set; }
+        public ListBox? ListBox { get; set; }
+
+        public void OnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add && ListBox?.Scroll is ScrollViewer scrollViewer)
+            {
+                scrollViewer.ScrollToEnd();
             }
         }
     }
